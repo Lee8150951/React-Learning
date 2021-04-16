4bb3a4b7c9a2cc7d052eca0df047a1a244610524
Lee8150951 <Liys9875@outlook.com>
2021-04-16 15:13:09 +0800

    day09

--- 16-pubsub/src/components/Search/index.jsx
+++ 16-pubsub/src/components/Search/index.jsx
@@ -1,34 +1,65 @@
 import React, {Component} from 'react';
 import PubSub from 'pubsub-js'
-import axios from "axios";
+// import axios from "axios";
 
 class Search extends Component {
   myRef = React.createRef()
 
-  search = () => {
+  search = async () => {
     const myRef = this.myRef.current.value
     // 发送请求前通知List更新状态
     PubSub.publish('flag', {
       isFirst: false,
       isLoading: true
     })
-    // 发送网络请求
-    axios.get(`/api/search/users?q=${myRef}`).then(
-      response => {
-        // 发送请求后通知List更新状态
-        PubSub.publish('flag', {
-          isLoading: false,
-          users: response.data.items
-        })
-      },
-      error => {
-        // 请求失败后的状态
-        PubSub.publish('flag', {
-          isLoading: false,
-          err: error.message
-        })
-      }
-    )
+    // 发送网络请求（使用axios方法）
+    // axios.get(`/api/search/users?q=${myRef}`).then(
+    //   response => {
+    //     // 发送请求后通知List更新状态
+    //     PubSub.publish('flag', {
+    //       isLoading: false,
+    //       users: response.data.items
+    //     })
+    //   },
+    //   error => {
+    //     // 请求失败后的状态
+    //     PubSub.publish('flag', {
+    //       isLoading: false,
+    //       err: error.message
+    //     })
+    //   }
+    // )
+
+    // 发送网络请求（使用fetch方法）(未优化)
+    // fetch('/api/search/users2?q=${myRef}').then(
+    //   response => {
+    //     console.log('success')
+    //     return response.json()
+    //   }
+    // ).then(
+    //   response => {
+    //     console.log('success', response)
+    //   }
+    // ).catch(
+    //   error => {
+    //     console.log('error', error)
+    //   }
+    // )
+
+    // 发送网络请求（使用fetch方法）(优化)
+    try {
+      const response = await fetch('/api/search/users2?q=${myRef}')
+      const data = await response.json()
+      PubSub.publish('flag', {
+        isLoading: false,
+        users: data.items
+      })
+    } catch (error) {
+      PubSub.publish('flag', {
+        isLoading: false,
+        err: error.message
+      })
+    }
   }
 
   render() {
